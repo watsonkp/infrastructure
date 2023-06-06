ffdaedc07d227d8e44a19e874ca287b3751a9aaa
Kevin Watson <git-contact@watsonkp.com>
2023-06-06 13:40:29 -0400

    Changed YouTrack backup to run every 5 minutes

--- git/backup/backup-youtrack.sh
+++ git/backup/backup-youtrack.sh
@@ -4,4 +4,8 @@ set -xe
 
 ping -c 3 $BACKUP_HOST
 
-scp -i /key/id_ed25519 -o UserKnownHostsFile=/config/known_hosts /opt/youtrack/backups/* $BACKUP_USER@$BACKUP_HOST:$BACKUP_OUTPUT
+while true
+do
+	scp -i /key/id_ed25519 -o UserKnownHostsFile=/config/known_hosts /opt/youtrack/backups/* $BACKUP_USER@$BACKUP_HOST:$BACKUP_OUTPUT
+	sleep 60
+done
