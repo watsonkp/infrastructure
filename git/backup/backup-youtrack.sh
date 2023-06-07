c4236f991534daa14928da2ab9e8db30092c6f47
Kevin Watson <git-contact@watsonkp.com>
2023-06-06 20:26:57 -0500

    Fixed YouTrack backup/restore scripts to handle empty directories
    The scp command produces an error when attempting to copy all files from an
    empty path. Check path for files and return early without an error if there are
    none.
    
    There seem to be inconsistencies Windows file path handling. Using Unix style
    "/" works.

--- git/backup/backup-youtrack.sh
+++ git/backup/backup-youtrack.sh
@@ -4,8 +4,8 @@ set -xe
 
 ping -c 3 $BACKUP_HOST
 
-while true
-do
-	scp -i /key/id_ed25519 -o UserKnownHostsFile=/config/known_hosts /opt/youtrack/backups/* $BACKUP_USER@$BACKUP_HOST:$BACKUP_OUTPUT
-	sleep 60
-done
+while
+	COUNT=$(ls /opt/youtrack/backups/ | wc -l)
+	test $COUNT != 0 || continue
+	scp -i /key/id_ed25519 -o UserKnownHostsFile=/config/known_hosts /opt/youtrack/backups/* "$BACKUP_USER@$BACKUP_HOST:$BACKUP_OUTPUT"
+do sleep 3600; done
--- git/backup/restore-youtrack.sh
+++ git/backup/restore-youtrack.sh
@@ -4,4 +4,6 @@ set -xe
 
 ping -c 3 $BACKUP_HOST
 
-scp -i /key/id_ed25519 -o UserKnownHostsFile=/config/known_hosts $BACKUP_USER@$BACKUP_HOST:$BACKUP_OUTPUT/* /opt/youtrack/backups/
+COUNT=$(ssh -i /key/id_ed25519 -o UserKnownHostsFile=/config/known_hosts "$BACKUP_USER@$BACKUP_HOST" "ls $BACKUP_OUTPUT" | wc -l)
+test $COUNT != 0 || return 0
+scp -i /key/id_ed25519 -o UserKnownHostsFile=/config/known_hosts "$BACKUP_USER@$BACKUP_HOST:$BACKUP_OUTPUT*" /opt/youtrack/backups/
