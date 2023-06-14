888fd17bbea0def9c8a8b67755f4b7571e448e04
Kevin Watson <git-contact@watsonkp.com>
2023-06-14 17:20:37 -0400

    Added initial PowerShell DSC configuration for a domain controller

---
+++ windows/DomainController.ps1
@@ -0,0 +1,37 @@
+﻿Configuration DC {
+    param(
+        [String]$domainName
+        [PSCredential]$domainCredential
+    )
+
+    Import-DscResource -ModuleName PsDesiredStateConfiguration
+    Import-DscResource -ModuleName IPAddressResource
+    Import-DscResource -ModuleName DomainControllerResource
+
+    $AllNodes | ForEach-Object {
+        Node $_.NodeName {
+            StaticIPAddress IP {
+                Ensure = 'Present'
+                IPAddress = $_.StaticIPAddress
+            }
+
+            WindowsFeature ADDS {
+                Ensure = 'Present'
+                Name = 'AD-Domain-Services'
+            }
+
+            WindowsFeatureSet ServicesAndTools {
+                Ensure = 'Present'
+                Name = @('DNS', 'RSAT-AD-PowerShell')
+            }
+
+            DomainControllerReplica DC {
+                DomainCredential = $domainCredential
+                DsrmPassword = $_.DsrmPassword
+                Ensure = 'Present'
+                DomainName = $domainName
+                ComputerName = $_.NodeName
+            }
+        }
+    }
+}
---
+++ windows/DomainControllerData.psd1
@@ -0,0 +1,11 @@
+@{
+    AllNodes = @(
+        @{
+            NodeName = ''
+            StaticIPAddress = ''
+            DsrmPassword = ''
+            PSDscAllowDomainUser = $true
+            PSDscAllowPlainTextPassword = $true
+        }
+    )
+}
